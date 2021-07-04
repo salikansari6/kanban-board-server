fd10029ac0c1a228a27a1dba06d9645c5efac28b
Salik <salik.ansari6@gmail.com>
2021-07-04 23:58:18 +0530

    added put route for tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,8 +3,29 @@ const TaskCollection = require("../models/TaskCollection");
 const router = require("express").Router();
 
 router.get("/:userId", async (req, res) => {
-  const result = await TaskCollection.find({ userId: userId });
-  console.log(result);
+  const result = await TaskCollection.find({ userId: req.params.userId });
+  res.send(result);
+});
+
+router.post("/:userId", async (req, res) => {
+  const newUserTasks = await TaskCollection.create({
+    userId: req.params.userId,
+    tasks: [
+      { title: "To-Do", columnColor: "red", items: [] },
+      { title: "In-Progress", columnColor: "yellow", items: [] },
+      { title: "Done", columnColor: "green", items: [] },
+    ],
+  });
+  res.send(newUserTasks);
+});
+
+router.put("/:userId", async (req, res) => {
+  console.log(req.body.tasks);
+  const updatedUserTasks = await TaskCollection.findOneAndUpdate(
+    { userId: req.params.userId },
+    { tasks: req.body.tasks }
+  );
+  res.send(updatedUserTasks);
 });
 
 module.exports = router;
